c492e91cc951fd779e5ab1a26e0f84575cd48b16
cwarre11 <69652418+cwarre11@users.noreply.github.com>
2022-04-19 15:14:51 -0400

    Update db.php

--- web250php/db.php
+++ web250php/db.php
@@ -1,11 +1,11 @@
  <?php
-$mysqli = new mysqli('mySQL', 'root', 'verysecret', 'Cars' );
+$mysqli = new mysqli('sql103.epizy.com', 'epiz_31507143', 'zuLWTijOReSqIL', 'epiz_31507143_cars' );
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 //select a database to work with
-$mysqli->select_db("Cars");
+$mysqli->select_db("cars");
  
 ?>
